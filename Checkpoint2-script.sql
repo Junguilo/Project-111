0f282f70c114af9bc125521983f1f1f528a53526
Bluegoob <52477695+Junguilo@users.noreply.github.com>
2024-11-11 21:37:12 -0800

    updated classes and got rid of '?' !!

--- Checkpoint2-script.sql
+++ Checkpoint2-script.sql
@@ -1,4 +1,4 @@
--- CREATE TABLE "User" (
+-- CREATE TABLE User (
 --     u_userkey DECIMAL(10, 0) NOT NULL PRIMARY KEY,
 --     u_name VARCHAR(152) NOT NULL,
 --     u_email VARCHAR(152) NOT NULL,
@@ -17,7 +17,6 @@
 -- CREATE TABLE HabitManager (
 --     hm_habitid DECIMAL(10, 0) NOT NULL PRIMARY KEY,
 --     hm_userkey DECIMAL(10, 0) NOT NULL, 
---     hm_dayshabit DECIMAL(10, 0) NOT NULL DEFAULT 7,
 --     hm_startdate DATE NOT NULL,
 --     hm_enddate DATE NOT NULL,
 --     hm_nonseq BOOLEAN NOT NULL DEFAULT FALSE,
@@ -49,15 +48,6 @@
 -- );
 
 
---User has Rest Days for their Exercise Habit, 
---They don’t want 7 days of logged Habit Logs? 
---(CREATE, nonseq & dayshabit)
-UPDATE HabitManager
-SET hm_dayshabit = ? --# of days the user wants to specify
-WHERE 
-    hm_userkey = ? AND -- userkey 
-    hm_nonseq = True;
-
 --User wants to update his email (UPDATE)
 UPDATE User
 SET u_email = "TestEmail@gmail.com"
@@ -81,32 +71,46 @@ WHERE
 
 --User wants to make a new Habit for study(CREATE) (seq habit)
 
-
 INSERT INTO HabitManager
-VALUES(1, 20, 3, "2024-12-08", "2024-12-14", FALSE, FALSE);
+VALUES(31, 1, "2024-12-08", "2024-12-14", "FALSE", "FALSE", 0);
 INSERT INTO StudyHabit
-VALUES(1, 1, "History", 60);
+VALUES(16, "US History" , 16, "History", 60);
+UPDATE StudyHabit
+SET sh_habitid = 31
+WHERE sh_shhabitid = 16;
 
 --User wants to make a new Habit for exercise (CREATE) (seq habit)
 INSERT INTO HabitManager
-VALUES(1, 20, 3, "2024-12-08", "2024-12-14", FALSE, FALSE);
+VALUES(32, 1, "2024-12-20", "2024-12-27", "FALSE", "TRUE", 0);
 INSERT INTO StudyHabit
-VALUES(1, 1, "History", 60);
+VALUES(16, 32, "US History" , 16, "History", 60);
 
 --For habit ID that has been finalized (end date has passed)
 -- see how the user did for the a specific task (RATIO OF status/total logs SELECT)
-UPDATE HabitManager
-SET hm_percentcompleted = (
-    SELECT COUNT(*) * 100.0 / (SELECT COUNT(*)
-                               FROM HabitLog
-                               WHERE HabitLog.hl_habitid = HabitManager.hm_habitid)
-    FROM HabitLog
-    WHERE HabitLog.hl_habitid = HabitManager.hm_habitid
-      AND HabitLog.hl_status = TRUE
+WITH temp_user AS (
+    SELECT 1 AS user_ID --1 WILL BE QUESTION MARK INSTEAD
+),
+temp_habit AS (
+    SELECT 1 AS habit_key --1 WILL BE QUESTION MARK INSTEAD
 )
-WHERE hm_userid = 1
-  AND hm_enddate < CURRENT_DATE;
-
+SELECT
+    (
+        SELECT COUNT()
+        FROM User, HabitManager, HabitLog, temp_user, temp_habit
+        WHERE temp_user.user_ID = User.u_userkey
+        AND User.u_userkey = HabitManager.hm_userkey
+        AND HabitManager.hm_habitid = temp_habit.habit_key
+        AND HabitManager.hm_habitid = HabitLog.hl_habitid
+        AND HabitLog.hl_status = 'TRUE'
+    ) 100.0 / 
+    (
+        SELECT COUNT(*)
+        FROM User, HabitManager, HabitLog, temp_user, temp_habit
+        WHERE temp_user.user_ID = User.u_userkey
+        AND HabitManager.hm_habitid = temp_habit.habit_key
+        AND User.u_userkey = HabitManager.hm_userkey
+        AND HabitManager.hm_habitid = HabitLog.hl_habitid
+    ) AS RATIO;
 
 --User wants to see the data of specific subjects
 -- that they studied after a certain date. (SELECT)
@@ -115,12 +119,18 @@ FROM User, HabitManager, StudyHabit
 WHERE 
     u_userkey = hm_userkey AND
     hm_habitid = sh_habitid AND
-    hm_enddate < ?; -- can be a date the user chooses. 
+    hm_enddate < CURRENT_DATE;
+    --hm_enddate < ?; -- can be a date the user chooses. 
 
 --Update non sequential to change date of habit must be done (UPDATE)
-
 UPDATE HabitLog
-SET hl_log_date = ?;
+SET hl_log_date = "2023-03-16"
+WHERE hl_logid = 10;
+
+-- UPDATE HabitLog
+-- SET hl_log_date = ?
+-- WHERE hl_logid = ?;
+
 
 --pull up users movie list
 --1)
@@ -146,36 +156,55 @@ AND HabitLog.hl_habitid = HabitManager.hm_habitid
 AND HabitManager.hm_habitid = ExerciseHabit.eh_ehhabitid;
 
 --User wants to add add a movie/anime thing to watch so the SQL makes a an ID for the new thing and sets the bool to false(user hasn't watched it yet) (CREATE)    (Needs python)
-INSERT INTO MediaWatched (mw_userkey, mw_mediatype, mw_title) VALUES (?, ?, ?);
+INSERT INTO MediaWatched (mw_userkey, mw_mediatype, mw_title) VALUES (1, "Documentary", "Jeffrey Dahmer Files");
+UPDATE MediaWatched
+SET mw_complete = "TRUE"
+WHERE 
+    mw_title = "Jeffrey Dahmer Files";
+--INSERT INTO MediaWatched (mw_userkey, mw_mediatype, mw_title) VALUES (?, ?, ?);
+
 
 --User wants to set some movie to watched (UPDATE) bool is set to true in db
 
 UPDATE MediaWatched
-SET mw_complete = TRUE
-WHERE mw_mediaid = ?;
+SET mw_complete = "TRUE"
+WHERE mw_mediaid = 30;
+
+-- UPDATE MediaWatched
+-- SET mw_complete = "TRUE"
+-- WHERE mw_mediaid = ?;
 
 --User wants to delete movie ID (DELETE) info about a specific movie is deleted
 
 DELETE FROM MediaWatched
-WHERE mw_mediaid = ?;
+WHERE mw_mediaid = 31;
+
+-- DELETE FROM MediaWatched
+-- WHERE mw_mediaid = ?;
 
 --User misclicked on the on set to true wants to set to not seen again (UPDATE) bool set to false again
 
 UPDATE MediaWatched
-SET mw_complete = FALSE
-WHERE mw_mediaid = ?;
+SET mw_complete = "FALSE"
+WHERE mw_mediaid = 30;
+--WHERE mw_mediaid = ?;
 
 --User wants to change schedule for non seq task (UPDATE)
 
 UPDATE HabitLog
-SET HabitLog.h1_Log_date = ?
-WHERE HabitLog.hl_logid = ? --log id we're changing
+SET hl_log_date = "2023-03-16"
+WHERE hl_logid = 26;--log id we're changing
+
+-- UPDATE HabitLog
+-- SET HabitLog.h1_Log_date = ?
+-- WHERE HabitLog.hl_logid = ?; --log id we're changing
 
 -- Set log for the day to complete (UPDATE)
 
 UPDATE HabitLog
-SET HabitLog.h1_status = TRUE
-WHERE HabitLog.hl_logid = ? ;
+SET hl_status = 'TRUE'
+WHERE hl_logid = 13;
+--WHERE HabitLog.hl_logid = ? ;
 
 --User wants to see the habits with durationmins < 60 for borth. (SELECT)
 
@@ -193,5 +222,6 @@ WHERE User.u_userkey = HabitManager.hm_userkey
 AND StudyHabit.sh_habitid= HabitManager.hm_habitid
 AND StudyHabit.sh_durationmin < 60;
 
+-- pull all user information
 SELECT *
 FROM User;
