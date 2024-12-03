9a3207394dd670caca45b8990d06dd89fd647678
Bluegoob <52477695+Junguilo@users.noreply.github.com>
2024-12-03 15:48:53 -0800

    create function

--- Checkpoint2-script.sql
+++ Checkpoint2-script.sql
@@ -53,207 +53,207 @@ CREATE TABLE ExerciseHabit (
 );
 
 
---User wants to update his email (UPDATE)
-UPDATE User
-SET u_email = "MaryClark@gmail.com"
-WHERE 
-    u_userkey = 20;
+-- --User wants to update his email (UPDATE)
+-- UPDATE User
+-- SET u_email = "MaryClark@gmail.com"
+-- WHERE 
+--     u_userkey = 20;
 
--- User wants to update his password (UPDATE)
-UPDATE User
-SET u_password = "Password123"
-WHERE 
-    u_userkey = 20;
+-- -- User wants to update his password (UPDATE)
+-- UPDATE User
+-- SET u_password = "Password123"
+-- WHERE 
+--     u_userkey = 20;
 
---User wants to look back on all his studyTasks
---that have a duration higher than 60 minutes (SELECT)
-SELECT sh_subject, sh_durationmin 
-FROM User, HabitManager, StudyHabit
-WHERE
-    u_userkey = hm_userkey AND
-    hm_habitid = sh_habitid AND
-    sh_durationmin > 60;
+-- --User wants to look back on all his studyTasks
+-- --that have a duration higher than 60 minutes (SELECT)
+-- SELECT sh_subject, sh_durationmin 
+-- FROM User, HabitManager, StudyHabit
+-- WHERE
+--     u_userkey = hm_userkey AND
+--     hm_habitid = sh_habitid AND
+--     sh_durationmin > 60;
 
---User wants to make a new Habit for study(CREATE) (seq habit)
+-- --User wants to make a new Habit for study(CREATE) (seq habit)
 
-INSERT INTO HabitManager (hm_userkey, hm_startdate, hm_enddate, hm_nonseq, hm_recurring, hm_percentcompleted)
-VALUES(1, "2024-12-08", "2024-12-14", "FALSE", "FALSE", 0);
-INSERT INTO StudyHabit (sh_title, sh_habitid, sh_subject, sh_durationmin)
-VALUES("US History" , 32, "History", 60);
+-- INSERT INTO HabitManager (hm_userkey, hm_startdate, hm_enddate, hm_nonseq, hm_recurring, hm_percentcompleted)
+-- VALUES(1, "2024-12-08", "2024-12-14", "FALSE", "FALSE", 0);
+-- INSERT INTO StudyHabit (sh_title, sh_habitid, sh_subject, sh_durationmin)
+-- VALUES("US History" , 32, "History", 60);
 
---User wants to make a new Habit for exercise (CREATE) (seq habit)
-INSERT INTO HabitManager (hm_userkey, hm_startdate, hm_enddate, hm_nonseq, hm_recurring, hm_percentcompleted)
-VALUES(2, "2024-12-20", "2024-12-27", "FALSE", "TRUE", 0);
-INSERT INTO ExerciseHabit (eh_habitid, eh_title, eh_activitytype, eh_durationmin)
-VALUES(33, "Push Day", "Weightlifting", 60);
+-- --User wants to make a new Habit for exercise (CREATE) (seq habit)
+-- INSERT INTO HabitManager (hm_userkey, hm_startdate, hm_enddate, hm_nonseq, hm_recurring, hm_percentcompleted)
+-- VALUES(2, "2024-12-20", "2024-12-27", "FALSE", "TRUE", 0);
+-- INSERT INTO ExerciseHabit (eh_habitid, eh_title, eh_activitytype, eh_durationmin)
+-- VALUES(33, "Push Day", "Weightlifting", 60);
 
---For habit ID that has been finalized (end date has passed)
--- see how the user did for the a specific task (RATIO OF status/total logs SELECT)
-WITH temp_user AS (
-    SELECT 1 AS user_ID --1 WILL BE QUESTION MARK INSTEAD
-),
-temp_habit AS (
-    SELECT 1 AS habit_key --1 WILL BE QUESTION MARK INSTEAD
-)
-SELECT
-    (
-        SELECT COUNT(*)
-        FROM User, HabitManager, HabitLog, temp_user, temp_habit
-        WHERE temp_user.user_ID = User.u_userkey
-        AND User.u_userkey = HabitManager.hm_userkey
-        AND HabitManager.hm_habitid = temp_habit.habit_key
-        AND HabitManager.hm_habitid = HabitLog.hl_habitid
-        AND HabitLog.hl_status = 'TRUE'
-    ) *100.0 / 
-    (
-        SELECT COUNT(*)
-        FROM User, HabitManager, HabitLog, temp_user, temp_habit
-        WHERE temp_user.user_ID = User.u_userkey
-        AND HabitManager.hm_habitid = temp_habit.habit_key
-        AND User.u_userkey = HabitManager.hm_userkey
-        AND HabitManager.hm_habitid = HabitLog.hl_habitid
-    ) AS RATIO;
+-- --For habit ID that has been finalized (end date has passed)
+-- -- see how the user did for the a specific task (RATIO OF status/total logs SELECT)
+-- WITH temp_user AS (
+--     SELECT 1 AS user_ID --1 WILL BE QUESTION MARK INSTEAD
+-- ),
+-- temp_habit AS (
+--     SELECT 1 AS habit_key --1 WILL BE QUESTION MARK INSTEAD
+-- )
+-- SELECT
+--     (
+--         SELECT COUNT(*)
+--         FROM User, HabitManager, HabitLog, temp_user, temp_habit
+--         WHERE temp_user.user_ID = User.u_userkey
+--         AND User.u_userkey = HabitManager.hm_userkey
+--         AND HabitManager.hm_habitid = temp_habit.habit_key
+--         AND HabitManager.hm_habitid = HabitLog.hl_habitid
+--         AND HabitLog.hl_status = 'TRUE'
+--     ) *100.0 / 
+--     (
+--         SELECT COUNT(*)
+--         FROM User, HabitManager, HabitLog, temp_user, temp_habit
+--         WHERE temp_user.user_ID = User.u_userkey
+--         AND HabitManager.hm_habitid = temp_habit.habit_key
+--         AND User.u_userkey = HabitManager.hm_userkey
+--         AND HabitManager.hm_habitid = HabitLog.hl_habitid
+--     ) AS RATIO;
 
---User wants to see the data of specific subjects
--- that they studied after a certain date. (SELECT)
-SELECT sh_subject
-FROM User, HabitManager, StudyHabit
-WHERE 
-    u_userkey = hm_userkey AND
-    hm_habitid = sh_habitid AND
-    hm_enddate < CURRENT_DATE;
-    --hm_enddate < ?; -- can be a date the user chooses. 
+-- --User wants to see the data of specific subjects
+-- -- that they studied after a certain date. (SELECT)
+-- SELECT sh_subject
+-- FROM User, HabitManager, StudyHabit
+-- WHERE 
+--     u_userkey = hm_userkey AND
+--     hm_habitid = sh_habitid AND
+--     hm_enddate < CURRENT_DATE;
+--     --hm_enddate < ?; -- can be a date the user chooses. 
 
---Update non sequential to change date of habit must be done (UPDATE)
-UPDATE HabitLog
-SET hl_log_date = "2023-03-16"
-WHERE hl_logid = 10;
-
+-- --Update non sequential to change date of habit must be done (UPDATE)
 -- UPDATE HabitLog
--- SET hl_log_date = ?
--- WHERE hl_logid = ?;
+-- SET hl_log_date = "2023-03-16"
+-- WHERE hl_logid = 10;
+
+-- -- UPDATE HabitLog
+-- -- SET hl_log_date = ?
+-- -- WHERE hl_logid = ?;
 
 
---pull up users movie list
---1)
-SELECT mw_title, MediaWatched.mw_complete
-FROM MediaWatched, User
-WHERE MediaWatched.mw_userkey = User.u_userkey AND
-    User.u_userkey = 1;
+-- --pull up users movie list
+-- --1)
+-- SELECT mw_title, MediaWatched.mw_complete
+-- FROM MediaWatched, User
+-- WHERE MediaWatched.mw_userkey = User.u_userkey AND
+--     User.u_userkey = 1;
 
---pull up study habit 
+-- --pull up study habit 
 
-SELECT *
-FROM User, HabitManager, HabitLog, StudyHabit
-WHERE User.u_userkey = HabitManager.hm_userkey 
-AND HabitLog.hl_habitid = HabitManager.hm_habitid
-AND HabitManager.hm_habitid = StudyHabit.sh_habitid;
+-- SELECT *
+-- FROM User, HabitManager, HabitLog, StudyHabit
+-- WHERE User.u_userkey = HabitManager.hm_userkey 
+-- AND HabitLog.hl_habitid = HabitManager.hm_habitid
+-- AND HabitManager.hm_habitid = StudyHabit.sh_habitid;
 
---pull up exercise
+-- --pull up exercise
 
-SELECT *
-FROM User, HabitManager, HabitLog, ExerciseHabit
-WHERE User.u_userkey = HabitManager.hm_userkey 
-AND HabitLog.hl_habitid = HabitManager.hm_habitid
-AND HabitManager.hm_habitid = ExerciseHabit.eh_ehhabitid;
+-- SELECT *
+-- FROM User, HabitManager, HabitLog, ExerciseHabit
+-- WHERE User.u_userkey = HabitManager.hm_userkey 
+-- AND HabitLog.hl_habitid = HabitManager.hm_habitid
+-- AND HabitManager.hm_habitid = ExerciseHabit.eh_ehhabitid;
 
---User wants to add add a movie/anime thing to watch so the SQL makes a an ID for the new thing and sets the bool to false(user hasn't watched it yet) (CREATE)    (Needs python)
-INSERT INTO MediaWatched (mw_userkey, mw_mediatype, mw_title, mw_complete) 
-VALUES (1, "Documentary", "Jeffrey Dahmer Files", "TRUE");
-UPDATE MediaWatched
-SET mw_complete = "TRUE"
-WHERE 
-    mw_title = "Jeffrey Dahmer Files";
---INSERT INTO MediaWatched (mw_userkey, mw_mediatype, mw_title) VALUES (?, ?, ?);
+-- --User wants to add add a movie/anime thing to watch so the SQL makes a an ID for the new thing and sets the bool to false(user hasn't watched it yet) (CREATE)    (Needs python)
+-- INSERT INTO MediaWatched (mw_userkey, mw_mediatype, mw_title, mw_complete) 
+-- VALUES (1, "Documentary", "Jeffrey Dahmer Files", "TRUE");
+-- UPDATE MediaWatched
+-- SET mw_complete = "TRUE"
+-- WHERE 
+--     mw_title = "Jeffrey Dahmer Files";
+-- --INSERT INTO MediaWatched (mw_userkey, mw_mediatype, mw_title) VALUES (?, ?, ?);
 
 
---User wants to set some movie to watched (UPDATE) bool is set to true in db
-
-UPDATE MediaWatched
-SET mw_complete = "TRUE"
-WHERE mw_mediaid = 30;
+-- --User wants to set some movie to watched (UPDATE) bool is set to true in db
 
 -- UPDATE MediaWatched
 -- SET mw_complete = "TRUE"
--- WHERE mw_mediaid = ?;
+-- WHERE mw_mediaid = 30;
 
---User wants to delete movie ID (DELETE) info about a specific movie is deleted
+-- -- UPDATE MediaWatched
+-- -- SET mw_complete = "TRUE"
+-- -- WHERE mw_mediaid = ?;
 
-DELETE FROM MediaWatched
-WHERE mw_mediaid = 31;
+-- --User wants to delete movie ID (DELETE) info about a specific movie is deleted
 
 -- DELETE FROM MediaWatched
--- WHERE mw_mediaid = ?;
+-- WHERE mw_mediaid = 31;
 
---User misclicked on the on set to true wants to set to not seen again (UPDATE) bool set to false again
+-- -- DELETE FROM MediaWatched
+-- -- WHERE mw_mediaid = ?;
 
-UPDATE MediaWatched
-SET mw_complete = "FALSE"
-WHERE mw_mediaid = 30;
---WHERE mw_mediaid = ?;
+-- --User misclicked on the on set to true wants to set to not seen again (UPDATE) bool set to false again
 
---User wants to change schedule for non seq task (UPDATE)
+-- UPDATE MediaWatched
+-- SET mw_complete = "FALSE"
+-- WHERE mw_mediaid = 30;
+-- --WHERE mw_mediaid = ?;
 
-UPDATE HabitLog
-SET hl_log_date = "2023-03-16"
-WHERE hl_logid = 26;--log id we're changing
+-- --User wants to change schedule for non seq task (UPDATE)
 
 -- UPDATE HabitLog
--- SET HabitLog.h1_Log_date = ?
--- WHERE HabitLog.hl_logid = ?; --log id we're changing
+-- SET hl_log_date = "2023-03-16"
+-- WHERE hl_logid = 26;--log id we're changing
 
--- Set log for the day to complete (UPDATE)
+-- -- UPDATE HabitLog
+-- -- SET HabitLog.h1_Log_date = ?
+-- -- WHERE HabitLog.hl_logid = ?; --log id we're changing
 
-UPDATE HabitLog
-SET hl_status = 'TRUE'
-WHERE hl_logid = 13;
---WHERE HabitLog.hl_logid = ? ;
+-- -- Set log for the day to complete (UPDATE)
 
---User wants to see the habits with durationmins < 60 for borth. (SELECT)
+-- UPDATE HabitLog
+-- SET hl_status = 'TRUE'
+-- WHERE hl_logid = 13;
+-- --WHERE HabitLog.hl_logid = ? ;
 
-SELECT ExerciseHabit.eh_title AS title, hm_percentcompleted AS percentdone
-FROM User, HabitManager, ExerciseHabit
-WHERE User.u_userkey = HabitManager.hm_userkey 
-AND ExerciseHabit.eh_habitid = HabitManager.hm_habitid
-AND ExerciseHabit.eh_durationmin < 60
+-- --User wants to see the habits with durationmins < 60 for borth. (SELECT)
 
-UNION ALL
+-- SELECT ExerciseHabit.eh_title AS title, hm_percentcompleted AS percentdone
+-- FROM User, HabitManager, ExerciseHabit
+-- WHERE User.u_userkey = HabitManager.hm_userkey 
+-- AND ExerciseHabit.eh_habitid = HabitManager.hm_habitid
+-- AND ExerciseHabit.eh_durationmin < 60
 
-SELECT StudyHabit.sh_title AS title, hm_percentcompleted AS percentdone
-FROM User, HabitManager, StudyHabit
-WHERE User.u_userkey = HabitManager.hm_userkey 
-AND StudyHabit.sh_habitid= HabitManager.hm_habitid
-AND StudyHabit.sh_durationmin < 60;
+-- UNION ALL
 
--- pull all user information
-SELECT *
-FROM User;
+-- SELECT StudyHabit.sh_title AS title, hm_percentcompleted AS percentdone
+-- FROM User, HabitManager, StudyHabit
+-- WHERE User.u_userkey = HabitManager.hm_userkey 
+-- AND StudyHabit.sh_habitid= HabitManager.hm_habitid
+-- AND StudyHabit.sh_durationmin < 60;
 
---Sequential SQL for the User, (Non Recurring)
--- if non Sequential = false, then add 7 
--- if the date is passed, log for it then set it for null
--- null, 0 , or 100 
---if non recurring, we dont care about null
--- if recurring we care about null, 
+-- -- pull all user information
+-- SELECT *
+-- FROM User;
 
--- REQUERYING SEQ
+-- --Sequential SQL for the User, (Non Recurring)
+-- -- if non Sequential = false, then add 7 
+-- -- if the date is passed, log for it then set it for null
+-- -- null, 0 , or 100 
+-- --if non recurring, we dont care about null
+-- -- if recurring we care about null, 
 
--- PART 1 EXTRACT THE 
+-- -- REQUERYING SEQ
 
-INSERT INTO HabitManager (
-    hm_userkey,
-    hm_startdate,
-    hm_enddate,
-    hm_nonseq, -- will be true
-    hm_recurring
-) 
-VALUES ( ?,?,?, "TRUE", ?));
+-- -- PART 1 EXTRACT THE 
 
---part 2 extract habit id created
---python code to get the habit id created habit_key = cursor.lastrowid
+-- INSERT INTO HabitManager (
+--     hm_userkey,
+--     hm_startdate,
+--     hm_enddate,
+--     hm_nonseq, -- will be true
+--     hm_recurring
+-- ) 
+-- VALUES ( ?,?,?, "TRUE", ?));
 
---part 3
---there will be a loop in pyton for the amount of days
+-- --part 2 extract habit id created
+-- --python code to get the habit id created habit_key = cursor.lastrowid
 
-INSERT INTO HabitLog (hl_habitid, hl_log_date) 
-         VALUES (?, ?)
+-- --part 3
+-- --there will be a loop in pyton for the amount of days
+
+-- INSERT INTO HabitLog (hl_habitid, hl_log_date) 
+--          VALUES (?, ?)
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify, request, render_template # Create our server, as well as sending/reading JSON
+from flask import Flask, url_for, redirect, jsonify, request, render_template # Create our server, as well as sending/reading JSON
 import sqlite3 
-#Using sqlite3 is a problem if we were to have more than 100 users
-#If we want to have a better system flask_alchemy is leagues better
+#Using sqlite3 is a problem if we were to have more than 100 users use it at the same time
+#If we want to have a better system flask_alchemy is leagues better, but sqlite3 is familiar
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super_secret_key'
 db = database = r"Checkpoint2-dbase.sqlite"
 
+## db Functions
 def openConnection(_dbFile):
     print("++++++++++++++++++++++++++++++++++")
     print("Open database: ", _dbFile)
@@ -34,6 +36,7 @@ def closeConnection(_conn, _dbFile):
 
     print("++++++++++++++++++++++++++++++++++")
 
+## Flask Functions
 @app.route('/')
 def index():
     conn = openConnection(db)
@@ -47,6 +50,10 @@ def index():
 
     return render_template('index.html', posts = sql)
 
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+
+    return render_template('create.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
